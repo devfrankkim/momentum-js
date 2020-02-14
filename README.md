f150af1153ff5da779c91b0f0b9f3880f6c52a30
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-02-13 23:22:03 -0500

    local storage completed

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Cloning MOMENTUM using Vanilla JavaScript 
+# Cloning MOMENTUM using Vanilla JavaScript
+
+Using "classList", you can add or remove a class without affecting any others the element may have. But if you assign "className", it will wipe out any existing classes while adding the new one (or if you assign an empty string it will wipe out all of them).
+
+Assigning "className" can be a convenience for cases where you are certain no other classes will be used on the element, but I would normally use the "classList" methods exclusively.
+
+And "classList" also has handy "toggle" and "replace" methods.
--- greeting.js
+++ greeting.js
@@ -1,13 +1,30 @@
-const form = document.querySelector(".js-form ");
+const form = document.querySelector(".js-form "); // input  - what's your name?
 const input = form.querySelector("input"); // input name
-const greeting = document.querySelector(".js-greetings");
-const USER_LOCAL_STROAGE = "currentUser";
+const greeting = document.querySelector(".js-greetings"); // painting greeting
+
+const USER_LOCAL_STROAGE = "currentUser"; // local storage
 const SHOWING_CLASSNAME = "showing";
 
-function paintGreeting(text) {
+function saveName(text) {
+  localStorage.setItem(USER_LOCAL_STROAGE, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CLASSNAME);
+  form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(currentValue) {
   form.classList.remove(SHOWING_CLASSNAME);
   greeting.classList.add(SHOWING_CLASSNAME);
-  greeting.innerText = `Hello ${text}`;
+  greeting.innerText = `Hello ${currentValue}`;
 }
 
 // querySelector => select the first one that he finds.
@@ -15,8 +32,10 @@ function paintGreeting(text) {
 
 function loadName() {
   const currentUser = localStorage.getItem(USER_LOCAL_STROAGE);
+
   if (currentUser === null) {
     // when there is no currentUser data on the local storage
+    askForName();
   } else {
     paintGreeting(currentUser);
   }
