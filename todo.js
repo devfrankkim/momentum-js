721ae1ef9994cd31b6e8aa16b71e50bcb5118636
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-02-14 22:47:33 -0500

    to do list on the way

---
+++ todo.js
@@ -0,0 +1,37 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+function paintToDo(text) {
+  //   console.log(text);
+  const ElLi = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "❌";
+  const span = document.createElement("span");
+  span.innerText = text;
+  ElLi.appendChild(span);
+  ElLi.appendChild(delBtn);
+  toDoList.appendChild(ElLi);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const toDos = localStorage.getItem(TODOS_LS);
+  if (toDos !== null) {
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
